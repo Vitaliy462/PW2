7e6d5f57a2dddcc909bd278a82bd7d93ad40e8c7
Vitaliy462 <lemeshvitaliy@gmail.com>
2021-12-27 20:19:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+parseFloat(3.45);
+parseInt(3.45); //преобразовывает строку в число ещё parseInt() == Math.floor()
+Math.floor(3.89); //3
+Math.ceil(3.3); //4
+Math.round(3.3); //3
+Math.round(3.6); //4
+
+console.log(parseInt('3.45px'))
+
+//ES5
+var count = 1
+//ES6
+let count1 = 2;
+const value = 0;
+
+// let str = 'SsAS '/sdsa/' '
+// let str1 = 'SsAS `sdsa` '
+
+//alert('Новая \n строка');
+
+// let n;
+// alert(n); // undefined
+
+//typeof() - результат типа данных
+
+
+//---
+
+let x = 5;
+//let userName = 'vitaliy';
+let cat = 
+{
+  name:'Floppa',
+  age:2
+}
+//alert(typeof(x));
+//or
+//alert(typeof x);
+//alert(typeof userName);
+alert(typeof cat);
